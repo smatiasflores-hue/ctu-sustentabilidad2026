153d026203cafbb04cdfc534ce7215d926af1d21
smatiasflores-hue <smatiasflores@gmail.com>
2026-09-16 03:49:21 -0300

    Update sustentabilidad.py

--- sustentabilidad.py
+++ sustentabilidad.py
@@ -364,7 +364,7 @@ try:
                     style_function=lambda x: {
                         "fillColor": "#1f77b4",
                         "color": "#0d3b66",
-                        "weight": 1.6,
+                        "weight": 1,
                         "fillOpacity": 0.6,
                     },
                     tooltip=(
